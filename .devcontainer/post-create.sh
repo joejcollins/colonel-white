871794e2289209abd27d976c89424988b08ff3f9
Joe J Collins <joejcollins@users.noreply.github.com>
2024-05-27 08:37:41 +0000

    Re order

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,7 +1,7 @@
 # Install the php vendor packages
 composer install
+# Use the development environment settings
+cp .env.example .env
 # Then generate an encryption key and the database
 php artisan key:generate
 php artisan migrate --force
-# Use the development environment settings
-cp .env.example .env
