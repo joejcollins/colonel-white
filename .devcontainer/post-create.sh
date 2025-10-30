adee85adc36a5a2c9fccf1e9b8f057541899743c
Joe J Collins <joejcollins@users.noreply.github.com>
2025-10-30 10:39:19 +0000

    Test Debian Trixie

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -9,6 +9,6 @@ make venv
 
 # Install Rust using rustup so we can have tex-fmt.
 # Once Debian trixie is available tex-fmt can be installed as a package.
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-. "$HOME/.cargo/env"
-cargo install tex-fmt
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+# . "$HOME/.cargo/env"
+# cargo install tex-fmt
