92b9072cae5ec5d85fc9d3f28da4086b722e1046
Joe J Collins <joejcollins@users.noreply.github.com>
2025-11-30 10:25:40 +0000

    Package install for just and tex-fmt

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,9 +6,3 @@ make venv
 
 # Install pre-commit hooks
 .venv/bin/pre-commit install
-
-# Install Rust using rustup so we can have tex-fmt.
-# Once Debian trixie is available tex-fmt can be installed as a package.
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-. "$HOME/.cargo/env"
-cargo install tex-fmt
